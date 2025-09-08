abfbc443ae3b0209fdc6b1f75611e770fefd5eb6
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2025-09-08 22:42:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.js - This is the main component that brings everything together
 import React from 'react';
 import CounterClass from './CounterClass.jsx';
 import CounterFunction from './CounterFunction.jsx';
@@ -7,10 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
-      {/* Main heading as required */}
       <h1 className="main-heading">Assignment-2 Counter Application</h1>
-      
-      {/* Container to show both components in a row */}
       <div className="counters-row">
         <CounterClass />
         <CounterFunction />
